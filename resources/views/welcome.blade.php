011ccf0054052e8c81ed8d7ab0d9bfc2cfa2235d
Angela Chan <a.chan@thetactilegroup.com>
2015-12-17 16:59:18 -0500

    update css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,9 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5
+                {{ time() }}
+            </div>
             </div>
         </div>
     </body>
